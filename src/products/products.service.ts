639e0a374f65ab599beb19d706f044b223e17b15
jrivas <josuerivas2311@gmail.com>
2025-01-15 18:58:11 -0600

    Create Product Images table and set creation

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -14,7 +14,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
-import { Product } from './entities/product.entity';
+import { ProductImage, Product } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -23,13 +23,25 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImgRepository: Repository<ProductImage>,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
-      const product = this.productRepository.create(createProductDto);
+      const { images = [], ...productDetails } = createProductDto;
+
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map((image) =>
+          this.productImgRepository.create({ url: image }),
+        ),
+      });
+
       await this.productRepository.save(product);
-      return product;
+
+      return { ...product, images };
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -65,6 +77,7 @@ export class ProductsService {
     const product = await this.productRepository.preload({
       id: id,
       ...updateProductDto,
+      images: [],
     });
 
     if (!product)
